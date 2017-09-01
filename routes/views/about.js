71c80f6e6aa23f45f8936841c11cc4dbece896ac
Trimikha Valentius <trimikha.valentius@ogilvy.com.au>
2017-09-02 00:04:24 +0800

    add Reza as contributor

--- routes/views/about.js
+++ routes/views/about.js
@@ -11,6 +11,7 @@ exports = module.exports = function(req, res) {
 	locals.organisers = [
 		{ name: 'Adi Setiawan', image: 'https://avatars3.githubusercontent.com/u/73311?v=4&s=400', twitter: 'adisetiawan', title: 'coordinator', profile: '/member/adi-setiawan' },
 		{ name: 'Trimikha Valentius', image: 'https://avatars2.githubusercontent.com/u/890146?v=4', twitter: 'tvalentius', title: 'coordinator', profile: '/member/trimikha-valentius' },
+		{ name: 'Reza Primasatya', image: 'https://avatars3.githubusercontent.com/u/4263809?v=4', twitter: 'rezaprimasatya', title: 'coordinator', profile: '/member/reza-primasatya' },
 	]
 
 	view.render('site/about');
